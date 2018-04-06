d8e2a39474abb258b679576cfe582715f669e86e
zzh <zzhlovesly@outlook.com>
2018-04-05 23:30:34 -0400

    Add test for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -12,7 +12,9 @@ const todos = [{
     text: 'First test todo',
 }, {
     _id: new ObjectID(),
-    text: 'Second test todo',
+    text: 'Second test todo V2',
+    completed: true,
+    completedAt: 123,
 }];
 
 // eslint-disable-next-line
@@ -145,6 +147,47 @@ describe('DELETE /toods/:id', () => {
         request(app)
             .delete('/todos/123')
             .expect(404)
+            .end(done);
+    });
+});
+
+describe('PATCH /todos/:id', () => {
+    it('should update the todo', (done) => {
+        const id = todos[0]._id.toHexString();
+        const text = 'Some bullshit new text';
+        request(app)
+            .patch(`/todos/${id}`)
+            .send({
+                text,
+                completed: true,
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(true);
+                expect(typeof res.body.todo.completedAt).toBe('number');
+            })
+            .end(done);
+        // my question is, sometimes we need to validate the res inside end block.
+        // we do another query to the db, then validate the data.
+        // sometimes we just validate them inside expect block. why?
+    });
+
+    it('should clear completedAt when todo is not completed', (done) => {
+        const id = todos[1]._id.toHexString();
+        const text = 'Some bullshit new text';
+        request(app)
+            .patch(`/todos/${id}`)
+            .send({
+                text,
+                completed: false,
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toBeNull();
+            })
             .end(done);
     });
 });
