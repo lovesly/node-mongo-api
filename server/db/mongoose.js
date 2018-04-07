274f81122edf287ca3cf2eb3c3fb329866df6b31
zzh <zzhlovesly@outlook.com>
2018-04-06 23:40:54 -0400

    Test heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // heroku mongodb add-on uri
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
     mongoose,
