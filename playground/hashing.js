84a899d922e9a4f85b9804ddfec0d8ade4b4a9e9
zzh <zzhlovesly@outlook.com>
2018-04-09 04:11:03 -0400

    Hash user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,19 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const password = '123abc';
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log('.............................');
+        console.log(hash);
+    });
+});
+
+const hashedPassword = '$2a$10$03EG3OZDNQZ7ydyBEntaBeYfHuBvcFWLQyH7o0xrdoaHSD9DbK0Jy';
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
 
 /**
  *  demo purpose
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 // schema??
 const UserSchema = new mongoose.Schema({
@@ -74,6 +75,22 @@ UserSchema.statics.findByToken = function(token) {
         'tokens.access': 'auth',
     });
 };
+
+// eslint-disable-next-line
+UserSchema.pre('save', function(next) {
+    const user = this;
+    if (user.isModified('password')) {
+        const { password } = user;
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
 
 const User = mongoose.model('User', UserSchema);
 
